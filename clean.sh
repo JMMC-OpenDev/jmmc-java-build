85e4dc000c3af70dbb1c50d0996c5d92ad691029
bourgesl <bourges.laurent@gmail.com>
2020-12-17 13:01:12 +0100

    fixed clean script

--- clean.sh
+++ clean.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 rm -rf oiexplorer-core jmal OITools testgui jmcs
-rm -rf aspro aspro-conf
-rm -rf oiexplorer
-rm -rf oimaging oimaging-uws-server
 
-rm -rf oiexplorer-existdb
-rm -rf exist
+rm -rf aspro aspro-conf
+rm -rf sclgui
+rm -rf oiexplorer
+rm -rf mfgui
+rm -rf oimaging oimaging-uws-server
+rm -rf smprsc smprun smptest
+
+rm -rf oiexplorer-existdb exist
 

8d63be8e19ddb9916a9725c83226e61d13dc02a8
bourgesl <bourges.laurent@gmail.com>
2021-06-03 22:31:26 +0200

    fixed jmmc_tools build (requires install, not verify)

--- build_gui.sh
+++ build_gui.sh
@@ -8,5 +8,5 @@ cd -
 
 # Build all modules (skip test)
 mvn process-resources
-mvn -Djarsigner.skip=true -Dmaven.javadoc.skip=true -Dmaven.test.skip=true verify
+mvn -Djarsigner.skip=true -Dmaven.javadoc.skip=true -Dmaven.test.skip=true install
 

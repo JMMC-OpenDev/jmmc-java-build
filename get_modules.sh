0902dac8b576ff6e9640c996f4c61a33dbf8cfb9
bourgesl <bourges.laurent@gmail.com>
2021-07-01 18:18:42 +0200

    fixed build_gui + use BASE_DIR in get_modules

--- get_modules.sh
+++ get_modules.sh
@@ -1,59 +1,61 @@
 #!/bin/bash
 
+BASE_URL=https://github.com/JMMC-OpenDev
+
 set -ux;
 
 # jmcs
-git clone --depth 1 https://github.com/JMMC-OpenDev/jmcs.git/
+git clone --depth 1 ${BASE_URL}/jmcs.git
 
 # oitools
 rm -rf oitools
-git clone --depth 1 https://github.com/JMMC-OpenDev/oitools.git
+git clone --depth 1 ${BASE_URL}/oitools.git
 
 # jmal
-git clone --depth 1 https://github.com/JMMC-OpenDev/jmal.git
+git clone --depth 1 ${BASE_URL}/jmal.git
 
 # oiexplorer core
-git clone --depth 1 https://github.com/JMMC-OpenDev/oiexplorer-core.git
+git clone --depth 1 ${BASE_URL}/oiexplorer-core.git
 
 
 # aspro-conf
-git clone --depth 1 https://github.com/JMMC-OpenDev/aspro-conf.git
+git clone --depth 1 ${BASE_URL}/aspro-conf.git
 
 # aspro gui
-git clone --depth 1 https://github.com/JMMC-OpenDev/aspro.git
+git clone --depth 1 ${BASE_URL}/aspro.git
 
 
 # searchcal gui
-git clone --depth 1 https://github.com/JMMC-OpenDev/sclgui.git
+git clone --depth 1 ${BASE_URL}/sclgui.git
 
 
 # oifits explorer gui
-git clone --depth 1 https://github.com/JMMC-OpenDev/oiexplorer.git
+git clone --depth 1 ${BASE_URL}/oiexplorer.git
 
 
 # litpro gui
-git clone --depth 1 https://github.com/JMMC-OpenDev/mfgui.git
+git clone --depth 1 ${BASE_URL}/mfgui.git
 
 
 # oimaging gui
-git clone --depth 1 https://github.com/JMMC-OpenDev/oimaging.git
+git clone --depth 1 ${BASE_URL}/oimaging.git
 
 # oimaging uws server
-git clone --depth 1 https://github.com/JMMC-OpenDev/oimaging-uws-server.git
+git clone --depth 1 ${BASE_URL}/oimaging-uws-server.git
 
 
 # app launcher config
-git clone --depth 1 https://github.com/JMMC-OpenDev/smprsc.git
+git clone --depth 1 ${BASE_URL}/smprsc.git
 
 # app launcher
-git clone --depth 1 https://github.com/JMMC-OpenDev/smprun.git
+git clone --depth 1 ${BASE_URL}/smprun.git
 
 # app launcher tester
-git clone --depth 1 https://github.com/JMMC-OpenDev/smptest.git
+git clone --depth 1 ${BASE_URL}/smptest.git
 
 
 # oidb deps
-# git clone --depth 1 https://github.com/JMMC-OpenDev/oiexplorer-existdb.git
+# git clone --depth 1 ${BASE_URL}/oiexplorer-existdb.git
 
 # oidb
 # git clone --depth 1 https://svn.jmmc.fr/jmmc-web/trunk/exist

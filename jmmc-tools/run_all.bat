613b05a004d98abc856bd28f093ea4feeeb61983
bourgesl <bourges.laurent@gmail.com>
2024-09-20 00:29:21 +0200

    fixed batch scripts

--- jmmc-tools/run_all.bat
+++ jmmc-tools/run_all.bat
@@ -6,17 +6,17 @@ REM Run all tools
 REM
 
 
-REM START /B run_AppLauncher-gui.bat
+REM START /MIN run_AppLauncher-gui.bat
 
-START /B run_Aspro2-gui.bat
-START /B run_SearchCal-gui.bat
+START /MIN run_Aspro2-gui.bat
+START /MIN run_SearchCal-gui.bat
 
-START /B run_OIFitsExplorer-gui.bat
+START /MIN run_OIFitsExplorer-gui.bat
 
-REM START /B run_LITpro-gui.bat
-REM START /B run_OImaging-gui.bat
+REM START /MIN run_LITpro-gui.bat
+REM START /MIN run_OImaging-gui.bat
 
-REM START /B run_OIFitsProcessor.bat
+REM START /MIN run_OIFitsProcessor.bat
 
 REM --- EoF ---
 

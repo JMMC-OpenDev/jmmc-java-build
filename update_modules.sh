463d41e0b0a1bb8f7de100d9f510a9b2e3f9fb8f
mellag <guillaume.mella@univ-grenoble-alpes.fr>
2024-09-20 09:43:52 +0200

    better output to follow module loop progression

--- update_modules.sh
+++ update_modules.sh
@@ -5,7 +5,8 @@ set -e
 
 for mod in jmcs jmal oitools oiexplorer-core aspro aspro-conf smprun smprsc smptest oimaging oimaging-uws-server mfgui oiexplorer sclgui # oiexplorer-existdb
 do
+  echo "Updating $mod module ..."
   cd $mod
   git pull
-  cd -
+  cd - > /dev/null
 done

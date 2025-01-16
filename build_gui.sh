bc6a531424ab6269773215071435ac41c5fadd91
mellag <guillaume.mella@univ-grenoble-alpes.fr>
2025-01-16 10:52:17 +0100

    fix unbound variable that throw an ERROR during CI action

--- build_gui.sh
+++ build_gui.sh
@@ -12,6 +12,7 @@ cd -
 
 # note: use insecure https as restlet https certificates are out-dated!
 #MVN_OPTS="-Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true"
+MVN_OPTS=""
 echo "MVN_OPTS: $MVN_OPTS"
 
 # Build all modules (skip test)
